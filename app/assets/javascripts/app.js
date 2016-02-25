35d1ff46ef14bb83e70641033aead1fd46500031
chan-d <chan.danny516@gmail.com>
2016-02-24 17:27:53 -0800

    added second to last video and styled buttons

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -30,7 +30,6 @@ $(document).ready(function(){
 			url: 'http://hummingbird.me/api/v1/anime/' + searched,
 			success: function(data) {	
 				var syn = data.synopsis.replace(/"/g, '&quot;');
-
 				$('#rendered_search').append(
 					//display search results
 					'<div class="col-md-12">' +
@@ -46,11 +45,6 @@ $(document).ready(function(){
 					'</div>' +
 					'</div>' +
 					'</div>' +
-
-
-					
-					 
-
 					//hidden form
 					'<form action="/movies" accept-charset="UTF-8" method="post">' +
 					'<input name="utf8" type="hidden" value="✓">' +
@@ -59,7 +53,7 @@ $(document).ready(function(){
 					'<input type="hidden" name="movie[j_title]" id="movie_j_title" value="'+ data.alternate_title + '">' +
 					'<input type="hidden" name="movie[synopsis]" id="movie_synopsis" value="'+ syn + '">' +
 					'<input type="hidden" name="movie[img_url]" id="movie_img_url" value="'+ data.cover_image + '">' +
-					'<input type="submit" name="commit" value="Add Movie">' +
+					'<input type="submit" name="commit" value="Add Movie" class="btn btn-default">' +
 					'</form>' + '</div>'
 					);
 				
